b914179291b116e693f44ff5d360181a8c3cfa66
125101308-DJ <233064462+125101308-DJ@users.noreply.github.com>
2025-10-07 20:37:19 +0100

    Update CustomErrorCodes.java
    Added descriptions for Enums

--- backend/cs6422/src/main/java/com/project/dine/right/enums/CustomErrorCodes.java
+++ backend/cs6422/src/main/java/com/project/dine/right/enums/CustomErrorCodes.java
@@ -1,5 +1,13 @@
 package com.project.dine.right.enums;
 
 public enum CustomErrorCodes {
-    SUCCESS, MISSING_REQUIRED_PARAMETER, UNAUTHORIZED, EMPTY_JSON, GENERIC_ERROR
+    SUCCESS("Success"),
+    MISSING_REQUIRED_PARAMETER("A required parameter is missing."),
+    UNAUTHORIZED("Unauthorized access"),
+    EMPTY_JSON("An empty request was provided"),
+    GENERIC_ERROR("General error");
+
+    CustomErrorCodes(String description) {
+
+    }
 }
